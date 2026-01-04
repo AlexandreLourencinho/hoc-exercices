65ebe4d8004b11224f7837c8bdc5d1d802aafd56
Alexandre LOURENCINHO <alexandre.lourencinho@cgi.com>
2026-01-05 00:31:26 +0100

    feat: added some comments and readme in common package

--- src/main/java/bed/hoc/exercice_hoc/common/constants/CommonConstants.java
+++ src/main/java/bed/hoc/exercice_hoc/common/constants/CommonConstants.java
@@ -1,5 +1,10 @@
 package bed.hoc.exercice_hoc.common.constants;
 
+/**
+ * this is a common utility class that I used for commodity. you can see some logic abstraction here
+ * there isn't much to learn here, it was just more comfortable for me instead of repeating the utility class
+ * private constructor throwing logic.
+ */
 public class CommonConstants {
 
     public static final String UTILITY_CLASS = "Utility class";
---
+++ src/main/java/bed/hoc/exercice_hoc/common/exceptions/readme.md
@@ -0,0 +1,3 @@
+# nothing here for now!
+
+there's nothing there for the level 1, you can ignore that part. it will change in other levels.
